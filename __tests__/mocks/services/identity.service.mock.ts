815a168f467400a568098a1c1633604c4f281a42
mark22312 <mark22312@gmail.com>
2019-12-14 23:07:35 +0200

    Fixed Tests

--- __tests__/mocks/services/identity.service.mock.ts
+++ __tests__/mocks/services/identity.service.mock.ts
@@ -1,6 +1,10 @@
-import { IdentityServiceInterface, LoginSuccessResponse, TryLogigArgs} from '../../../src/services/identity.service'
 import { AxiosInstance, AxiosResponse } from "axios";
+import { IdentityServiceInterface, LoginSuccessResponse, TryLogigArgs} from '../../../src/services/identity.service'
 
 export class IdentityServiceMock implements IdentityServiceInterface {
 	public preformLogin = jest.fn();
+	public getUser = jest.fn();
+	public getTokenFromStorage = jest.fn();
+	public setTokenToStorage = jest.fn();
+	public logout = jest.fn();
 }
