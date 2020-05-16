5d2d902fb28f666b2bcc64d3268d83f70cf3d377
mark22312 <mark22312@gmail.com>
2020-05-16 17:12:08 +0300

    Fixed config

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,1 +1,1 @@
-export const BASE_API_URL: string = process.env.REACT_BASE_API_URL || 'http://localhost:4000';
+export const BASE_API_URL: string = process.env.REACT_APP_BASE_API_URL || 'http://localhost:4000';
