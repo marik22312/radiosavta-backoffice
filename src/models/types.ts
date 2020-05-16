f88532056a3f1233d532d98b0809a79434850e56
Marik Shnitman <mariks@wix.com>
2020-05-16 16:52:41 +0300

    Feature/types (#9)

--- src/models/types.ts
+++ src/models/types.ts
@@ -1,22 +1,41 @@
 export interface ModelWithTimestamps {
-	created_at: string;
-	updated_at: string;
+  created_at: string;
+  updated_at: string;
 }
 
 export interface IUser extends ModelWithTimestamps {
-	id: string;
+  id: number;
+  name: string;
+  quote: string;
+  location: string;
+  profile_image: string;
+  email: string;
+}
+export interface IFullUser extends IUser {
+  programs: IProgram[];
+}
+
+
+export interface IRecordedShow extends ModelWithTimestamps {
+	id: number;
+	program_id: number;
+	url: string;
 	name: string;
-	quote: string;
-	location: string;
-	profile_image: string;
-	email: string;
+	duration: string;
+	is_displayed: boolean;
+	recorded_at: string;
 }
 
 export interface IProgram extends ModelWithTimestamps {
-	id: string;
-	name_en: string;
-	name_he?: string;
-	description: string;
-	cover_image: string;
-	is_displayed?: boolean
+  id: number;
+  name_en: string;
+  name_he?: string;
+  description: string;
+  cover_image: string;
+  is_displayed?: boolean;
+}
+
+export interface IFullProgram extends ModelWithTimestamps, IProgram {
+	users: IUser[];
+	recorded_shows: IRecordedShow[];
 }
