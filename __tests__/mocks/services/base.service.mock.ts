06fd8ae87d321aa2747de522e730df03f97825ec
mark22312 <mark22312@gmail.com>
2020-05-25 19:16:38 +0300

    Added programs store

--- __tests__/mocks/services/base.service.mock.ts
+++ __tests__/mocks/services/base.service.mock.ts
@@ -1,16 +1,23 @@
 import { IBaseApiService } from '../../../src/services/base.api.service';
+import { IProgramsService } from '../../../src/services/programs.service';
 import { IUsersService } from '../../../src/services/users.service';
 
-export const ApiServiceMock = (): IBaseApiService => ({
+export const ApiServiceMock = () => ({
 	get: jest.fn(),
 	post: jest.fn(),
 	put: jest.fn(),
 	delete: jest.fn(),
 })
 
-export class UsersServiceMock implements IUsersService {
-	public getAllUsers = jest.fn();
-	public getUserById = jest.fn();
-	public updateUserById = jest.fn();
-	public disableUser = jest.fn();
-}
+export const UsersServiceMock = () =>  ({
+	getAllUsers: jest.fn(),
+	getUserById: jest.fn(),
+	updateUserById: jest.fn(),
+	disableUser: jest.fn(),
+})
+export const ProgramsServiceMock = () =>  ({
+	getAllPrograms: jest.fn(),
+	getProgramById: jest.fn(),
+	updateProgramById: jest.fn(),
+	disableProgram: jest.fn(),
+})
---
+++ src/stores/programs.store.spec.ts
@@ -0,0 +1,40 @@
+import Chance from "chance";
+
+import { mockProgram } from "../../__tests__/mocks/data.mock";
+import { ProgramsServiceMock } from "../../__tests__/mocks/services/base.service.mock";
+import ProgramsStore from "./programs.store";
+
+const chance = Chance();
+
+describe("Program", () => {
+	let programsServiceMock: any;
+	let programsStore: ProgramsStore;
+	beforeEach(() => {
+		programsServiceMock = ProgramsServiceMock();
+	})
+  it("Should be a program", async () => {
+    const program = mockProgram();
+    let externalPromise: any;
+
+    programsServiceMock.getAllPrograms.mockImplementation(
+      () =>
+        new Promise(res => {
+          externalPromise = res;
+        })
+    );
+
+    programsStore = new ProgramsStore(programsServiceMock);
+
+    expect(programsStore.isLoading).toBe(false);
+
+    const response = programsStore.fetchAll();
+
+    expect(programsStore.isLoading).toBe(true);
+
+    externalPromise([program]);
+
+    expect(programsServiceMock.getAllPrograms).toBeCalledTimes(1);
+    expect(await response).toStrictEqual([program]);
+    expect(programsStore.isLoading).toBe(false);
+  });
+});
