1836e875f4d048aaf08fb7ccc2ba189bb9d78e87
mark22312 <mark22312@gmail.com>
2020-06-14 23:57:29 +0300

    added error

--- src/pages/protected/users-page/users.page.tsx
+++ src/pages/protected/users-page/users.page.tsx
@@ -3,12 +3,23 @@ import * as React from "react";
 import { RouteComponentProps } from "react-router-dom";
 import IdentityStore from "../../../stores/identity.store";
 
-import { Col, Container, Row, FormGroup, Label, Input, Form, Button } from "reactstrap";
+import {
+  Col,
+  Container,
+  Row,
+  FormGroup,
+  Label,
+  Input,
+  Form,
+  Button,
+  Spinner,
+  Alert
+} from "reactstrap";
 import { Page } from "../../../components/Page/Page";
 import { IUser } from "../../../models/types";
 import UsersStore from "../../../stores/users.store";
 import { Card } from "../../../components/Card/Card";
-import { Formik, FormikProps } from 'formik';
+import { Formik, FormikProps } from "formik";
 
 interface Props extends RouteComponentProps {
   identityStore: IdentityStore;
@@ -16,6 +27,8 @@ interface Props extends RouteComponentProps {
 }
 
 interface State {
+  isLoading: boolean;
+  error: string | null;
   users: IUser[];
   fileToUpload: any;
   fileUrl: string;
@@ -28,9 +41,11 @@ export class UsersPage extends React.Component<Props, State> {
     super(props);
 
     this.state = {
-	  users: [],
-	  fileUrl: '',
+      users: [],
+      fileUrl: "",
 	  fileToUpload: null,
+	  isLoading: false,
+	  error: null
     };
   }
 
@@ -39,40 +54,51 @@ export class UsersPage extends React.Component<Props, State> {
   }
 
   private async onFormSubmit(values: any) {
-	const {fileToUpload} = this.state;
-	const newValues = {
-		...values,
-		profile_picture: fileToUpload
-	}
-	try{
-		const response = await this.props.usersStore.createUser(newValues);
-	} catch(err) {
-		console.log(err)
-	}
+	  this.setState({
+		  isLoading: true,
+		  error: null,
+	  })
+    const { fileToUpload } = this.state;
+    const newValues = {
+      ...values,
+      profile_picture: fileToUpload
+    };
+    try {
+	  await this.props.usersStore.createUser(newValues);
+	  return this.setState({
+		isLoading: false,
+		error: null,
+	  })
+    } catch (err) {
+      this.setState({
+		  isLoading: false,
+		  error: err.message
+	  })
+    }
   }
 
   private onFileChanged(event: any) {
-	// const newFile = new File(event.target.files[0], '')
-	const newFile = URL.createObjectURL(event.target.files[0])
-	this.setState({
-		fileUrl: URL.createObjectURL(event.target.files[0]),
-		fileToUpload: event.target.files[0],
-	})
+    // const newFile = new File(event.target.files[0], '')
+    const newFile = URL.createObjectURL(event.target.files[0]);
+    this.setState({
+      fileUrl: URL.createObjectURL(event.target.files[0]),
+      fileToUpload: event.target.files[0]
+    });
   }
 
   private renderCreateUserForm(props: FormikProps<any>) {
     return (
       <>
         <Col xs={6}>
-			<img src={this.state.fileUrl} />
+          <img src={this.state.fileUrl} />
           <FormGroup>
             <Label>Full name</Label>
             <Input
               type="text"
               name="fullname"
               id="fullname"
-			  placeholder="John Smith"
-			  onChange={props.handleChange}
+              placeholder="John Smith"
+              onChange={props.handleChange}
             />
           </FormGroup>
         </Col>
@@ -82,12 +108,11 @@ export class UsersPage extends React.Component<Props, State> {
             <Input
               type="file"
               accept="image/jpeg image/png"
-			  name="profile_picture"
-			  onChange={e => {
-				  this.onFileChanged(e);
-				  props.handleChange(e);
-				}
-			}
+              name="profile_picture"
+              onChange={e => {
+                this.onFileChanged(e);
+                props.handleChange(e);
+              }}
             />
           </FormGroup>
         </Col>
@@ -98,8 +123,8 @@ export class UsersPage extends React.Component<Props, State> {
               type="email"
               name="email"
               placeholder="Johns@radiosavta.com"
-			  autoComplete="off"
-			  onChange={props.handleChange}
+              autoComplete="off"
+              onChange={props.handleChange}
             />
           </FormGroup>
         </Col>
@@ -109,26 +134,40 @@ export class UsersPage extends React.Component<Props, State> {
             <Input
               type="text"
               name="location"
-			  placeholder="Mitse Ramon, Israel"
-			  onChange={props.handleChange}
+              placeholder="Mitse Ramon, Israel"
+              onChange={props.handleChange}
             />
           </FormGroup>
         </Col>
         <Col xs={6}>
           <FormGroup>
             <Label>Password</Label>
-            <Input type="password" placeholder="Password" autoComplete="off" onChange={props.handleChange} name="password"/>
+            <Input
+              type="password"
+              placeholder="Password"
+              autoComplete="off"
+              onChange={props.handleChange}
+              name="password"
+            />
           </FormGroup>
         </Col>
         <Col xs={6}>
           <FormGroup>
             <Label>Show on broadcasters page?</Label>
-            <Input type="checkbox" onChange={props.handleChange} name="showOnWebsite"/>
+            <Input
+              type="checkbox"
+              onChange={props.handleChange}
+              name="showOnWebsite"
+            />
           </FormGroup>
         </Col>
-		<Col xs={12}>
-			<Button color="primary" onClick={props.handleSubmit}>Submit</Button>
-		</Col>
+        <Col xs={12}>
+			{this.state.isLoading && <Spinner color="primary"/>}
+          <Button color="primary" onClick={props.handleSubmit} disabled={this.state.isLoading}>
+            Submit
+          </Button>
+		  {this.state.error && <Alert color="danger">{this.state.error}</Alert>}
+        </Col>
       </>
     );
   }
@@ -151,10 +190,12 @@ export class UsersPage extends React.Component<Props, State> {
               <Card>
                 <Card.Content>
                   <Row>
-				  <Formik
-				  initialValues={{}}
-				  render={props => this.renderCreateUserForm(props)}
-				  onSubmit={values => this.onFormSubmit(values)}/></Row>
+                    <Formik
+                      initialValues={{}}
+                      render={props => this.renderCreateUserForm(props)}
+                      onSubmit={values => this.onFormSubmit(values)}
+                    />
+                  </Row>
                 </Card.Content>
               </Card>
             </Container>
