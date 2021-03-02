f446745dd52ef3976a19c676dbda77f024d4b70f
Dudiko2 <38940311+Dudiko2@users.noreply.github.com>
2021-03-02 20:10:49 +0200

    Added Edit user modal (#44)

---
+++ src/components/EditUserModal/EditUserModal.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { Modal, Input, Form } from "antd";
+
+import { useUpdateUser } from "../../hooks/useUpdateUser";
+
+import { IFullUser } from "../../models/types";
+
+interface ModalProps {
+  isOpen: boolean;
+  closeModal: () => void;
+  user: IFullUser;
+}
+
+const EditUserModal: React.FC<ModalProps> = ({ isOpen, closeModal, user }) => {
+  const [formInstance] = Form.useForm();
+  const [updateUser, { isLoading: isUpdating }] = useUpdateUser();
+
+  const closeAndReset = () => {
+    closeModal();
+    formInstance.resetFields();
+  };
+
+  const submitModal = async () => {
+    const values = await formInstance.validateFields();
+
+    updateUser(
+      { userId: user.id, data: values },
+      {
+        onSuccess: () => closeModal(),
+      }
+    );
+  };
+
+  return (
+    <Modal
+      title="Edit user"
+      visible={isOpen}
+      onCancel={closeAndReset}
+      onOk={submitModal}
+      zIndex={1001} // because UserSidePanel is 1000
+      closable
+      confirmLoading={isUpdating}
+    >
+      <Form
+        form={formInstance}
+        name="editUserForm"
+        initialValues={{ name: user.name, location: user.location }}
+      >
+        <Form.Item name="name" rules={[{ type: "string", min: 3 }]}>
+          <Input placeholder={"Enter name..."} />
+        </Form.Item>
+        <Form.Item name="location" rules={[{ type: "string", min: 1 }]}>
+          <Input placeholder={"Enter location..."} />
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+};
+
+export default EditUserModal;
