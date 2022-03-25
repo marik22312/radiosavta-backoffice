4e9fa77e303551862b0c48015df91e6052cc4bbc
mark22312 <mark22312@gmail.com>
2022-03-25 22:44:24 +0300

    Added edit image from my profile page

--- src/pages/protected/profile/ProfilePage.tsx
+++ src/pages/protected/profile/ProfilePage.tsx
@@ -3,10 +3,34 @@ import { useLoggedInUser } from "../../../hooks/auth/useLoggedInUser";
 import { UserPage } from "../../../components/UserPage/UserPage";
 import { useState } from "react";
 import EditUserModal from "../../../components/EditUserModal/EditUserModal";
+import { EditImageModal } from "../../../components/EditImageImageModal/EditImageImageModal";
+import { useEditUserImage } from "../../../components/UserSidePanel/hooks/useEditUserImage";
 
 export const ProfilePage: React.FC = () => {
   const { user, isLoading, refetch } = useLoggedInUser();
   const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [errorMessage, setErrorMessage] = useState<string>();
+  const [isEditImageModalOpen, setIsEditImageModalOpen] = useState(false);
+
+  const { isLoading: isImageLoading, updateImage } = useEditUserImage(
+    user?.id ?? "",
+    {
+      onError: (err) =>
+        setErrorMessage("Something went wrong, please refresh and try again"),
+      onSuccess: () => {
+        setIsEditImageModalOpen(false);
+        refetch();
+      },
+    }
+  );
+
+  const onEditImage = (image: File) => {
+    updateImage(image);
+  };
+
+  const onCancelEditImage = () => {
+    setIsEditImageModalOpen(false);
+  };
   const onUserUpdated = () => {
     refetch();
   };
@@ -23,6 +47,7 @@ export const ProfilePage: React.FC = () => {
         title="My Profile"
         user={user}
         onClickEditUser={onClickEditUser}
+        onClickEditUserImage={() => setIsEditImageModalOpen(true)}
       />
       {!!user && (
         <EditUserModal
@@ -30,6 +55,15 @@ export const ProfilePage: React.FC = () => {
           closeModal={() => setIsEditModalOpen(false)}
           user={user}
           onUserUpdated={onUserUpdated}
+        />
+      )}
+      {!!user && (
+        <EditImageModal
+          isOpen={isEditImageModalOpen}
+          onOk={onEditImage}
+          onCancel={onCancelEditImage}
+          isLoading={isLoading}
+          errorMessage={errorMessage}
         />
       )}
     </>
