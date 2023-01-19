2a7ef9584a2fa5970f4f5f3844e674925e42fad2
Marik Shnitman <mark22312@gmail.com>
2023-01-19 22:13:37 +0200

    Added sentry (#109)

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,19 @@ import App from "./App";
 import "./index.scss";
 import * as serviceWorker from "./serviceWorker";
 
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
